04d928cad1eac46860f19ed5e1d5fb03de294c4a
Dawid Fourie <dawief4@gmail.com>
2023-07-26 13:55:05 -0700

    some new code test 1.11

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import express from "express"
 import cors from 'cors'
 // import jwt from 'jsonwebtoken'
 // import bcrypt from 'bcryptjs'
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 
 const app = express()
-const PORT = 4000
+dotenv.config()
+const PORT = process.env.PORT || 5000
 app.use(cors())
 
 
