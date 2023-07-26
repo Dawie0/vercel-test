52107e3c00cf33a5acaa1089f38c673b222f20c7
Dawid Fourie <dawief4@gmail.com>
2023-07-26 13:15:44 -0700

    some new code test 1.2

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ import dotenv from 'dotenv'
 const app = express()
 const PORT = 4000
 
-dotenv.config()
 
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
